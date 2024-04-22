fc88b8549e0c0b095de60ac3ce8175143c9a8f48
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-04-22 15:04:28 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let text = document.getElementById('text')
+let suz_background = prompt("Enter body backgroundColor")
+let suz_text_color = prompt('Enter text color')
+
+text.style.backgroundColor = suz_background
+text.style.color = suz_text_color
